3b3c687ee1615c9a11bc6f20289ce74498cd3568
@jerscape <jeremydutton5@gmail.com>
2025-10-02 12:48:05 -0400

    modal click boolean working

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,27 +1,43 @@
 import Post from "./Post";
-import NewPost from './NewPost'
-import classes from './PostsList.module.css';
-import { useState } from 'react';
+import NewPost from "./NewPost";
+import Modal from "./Modal";
+import classes from "./PostsList.module.css";
+import { useState } from "react";
 
-function PostsList(){
-  const [enteredBody, setEnteredBody] = useState('');
+function PostsList() {
+  const [modalIsVisible, setModalIsVisible] = useState(true);
+  const [enteredBody, setEnteredBody] = useState("");
+  const [enteredAuthor, setEnteredAuthor] = useState("");
 
-  function bodyChangeHandler(event){
-    setEnteredBody(event.target.value)
+  function hideModalHandler() {
+    setModalIsVisible(false);
   }
 
-  return(
-    
-    <>
-    <NewPost onBodyChange={bodyChangeHandler} /> 
-      <ul className={classes.posts}>
-       <Post author="Maximillian" body={enteredBody} />
-       <Post author="Manuel" body ="Check out the full course!"/>
-     </ul>
-      </>
-  )
-   
+  function bodyChangeHandler(event) {
+    setEnteredBody(event.target.value);
+  }
 
+  function authorChangeHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
+  return (
+    <>
+      {modalIsVisible && (
+        <Modal onClose={hideModalHandler}>
+          <NewPost
+            onBodyChange={bodyChangeHandler}
+            onAuthorChange={authorChangeHandler}
+          />
+        </Modal>
+      )}
+
+      <ul className={classes.posts}>
+        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Manuel" body="Check out the full course!" />
+      </ul>
+    </>
+  );
 }
 
-export default PostsList
+export default PostsList;
---
+++ src/components/Probe.jsx
@@ -0,0 +1,8 @@
+// CssProbe.jsx
+import classes from "./Modal.module.css";
+
+export default function CssProbe() {
+  return <div className={classes.backdrop}>probe</div>;
+}
+
+
